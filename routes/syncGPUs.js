5e05593bfe969f732aa0d341f21e8de596cc9647
BeomJuGo <lom0097@naver.com>
2025-10-13 17:05:35 +0900

    Update syncGPUs.js

--- routes/syncGPUs.js
+++ routes/syncGPUs.js
@@ -94,7 +94,7 @@ async function fetchGptSummary(name) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "gpt-3.5-turbo",
+          model: "gpt-4",
           messages: [
             {
               role: "user",
